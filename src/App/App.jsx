0781b504764639cbd0e351a680444e343f419270
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-09-16 19:59:30 +0200

    Initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ContactFrom from '../components/ContactForm/ContactForm';
+import ContactList from '../components/ContactList/ContactList';
+import Filter from '../components/Filter/Filter';
+import { Loader } from 'components/Loader/Loader';
+import { Container, Title, SubTitle } from './App.styled';
+import { useSelector } from 'react-redux';
+import { selectContacts, selectIsLoading, selectError } from 'redux/selectors';
+
+export const App = () => {
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      {error && (
+          <p>Whoops, something went wrong: {error}</p>
+        )}
+      <ContactFrom />
+      <SubTitle>Contacts</SubTitle>
+      {contacts.length > 0 && <Filter />}
+      {!isLoading && <Loader />}
+      <ContactList />
+    </Container>
+  );
+};
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { List, Button, Item } from './ContactList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContactsThunk } from 'redux/contactsThunk';
+import { selectFilteredContacts } from 'redux/selectors';
+
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const filteredContacts = useSelector(selectFilteredContacts);
+
+  return (
+    <>
+      <List>
+        {filteredContacts.map(({ id, name, phone }) => (
+          <Item key={id}>
+            {name + ' : ' + phone}
+            {
+              <Button
+                type="button"
+                name="delete"
+                onClick={() => dispatch(deleteContactsThunk(id))}
+              >
+                delete
+              </Button>
+            }
+          </Item>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default ContactList;
