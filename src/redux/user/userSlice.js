9c1f3213d5b82d3e55ae24a719a99fbf4d0cdfcc
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-09-18 15:28:32 +0200

    updated store

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -6,6 +6,7 @@ import {
 } from './userThunk';
 
 const { createSlice } = require('@reduxjs/toolkit');
+
 const initialState = {
   user: null,
   token: null,
@@ -22,50 +23,80 @@ const handleIfReject = (state, { payload }) => {
   state.error = payload;
 };
 
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState,
+//   extraReducers: builder => {
+//     builder
+//       .addCase(logoutThunk.fulfilled, () => {
+//         return initialState;
+//       })
+//       .addCase(refreshUserThunk.fulfilled, (state, { payload }) => {
+//         state.user = payload;
+//         state.isLoading = false;
+//         state.error = null;
+//       })
+//       .addMatcher(
+//         isAnyOf(signUpThunk.fulfilled, loginThunk.fulfilled),
+//         (state, { payload }) => {
+//           state.user = payload;
+//           state.isLoading = false;
+//           state.error = null;
+//         }
+//       )
+//       .addMatcher(
+//         isAnyOf(
+//           signUpThunk.pending,
+//           loginThunk.pending,
+//           logoutThunk.pending,
+//           refreshUserThunk.pending
+//         ),
+//         handleIfPending
+//       )
+//       .addMatcher(
+//         isAnyOf(
+//           signUpThunk.rejected,
+//           loginThunk.rejected,
+//           logoutThunk.rejected,
+//           refreshUserThunk.rejected
+//         ),
+//         handleIfReject
+//       );
+//   },
+// });
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: builder => {
     builder
+      .addCase(signUpThunk.pending, handleIfPending)
+      .addCase(signUpThunk.rejected, handleIfReject)
       .addCase(signUpThunk.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoading = false;
         state.error = null;
       })
+      .addCase(loginThunk.pending, handleIfPending)
+      .addCase(loginThunk.rejected, handleIfReject)
       .addCase(loginThunk.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoading = false;
         state.error = null;
       })
+      .addCase(logoutThunk.pending, handleIfPending)
+      .addCase(logoutThunk.rejected, handleIfReject)
       .addCase(logoutThunk.fulfilled, () => {
         return initialState;
       })
+      .addCase(refreshUserThunk.pending, handleIfPending)
+      .addCase(refreshUserThunk.rejected, handleIfReject)
       .addCase(refreshUserThunk.fulfilled, (state, { payload }) => {
         state.user = payload;
         state.isLoading = false;
         state.error = null;
-      })
-      .addMatcher(
-        isAnyOf(
-          signUpThunk.pending,
-          loginThunk.pending,
-          logoutThunk.pending,
-          refreshUserThunk.pending
-        ),
-        handleIfPending
-      )
-      .addMatcher(
-        isAnyOf(
-          signUpThunk.rejected,
-          loginThunk.rejected,
-          logoutThunk.rejected,
-          refreshUserThunk.rejected
-        ),
-        handleIfReject
-      );
+      });
   },
 });
-
 export const authReducer = authSlice.reducer;
