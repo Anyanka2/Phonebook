4e91f193a9b3842eda8b68ff509163059bae8832
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-09-18 22:34:24 +0000

    Deploying to gh-pages from @ Anyanka2/goit-react-hw-08-phonebook@967988e078c63ffee15f85f43ad313cb902b237d 🚀

---
+++ static/js/680.a7e677c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,n,h){h.r(n);h(2791);var o=h(3329);n.default=function(){return(0,o.jsx)("div",{children:(0,o.jsx)("h1",{children:"PHONEBOOK"})})}}}]);
+//# sourceMappingURL=680.a7e677c3.chunk.js.map
