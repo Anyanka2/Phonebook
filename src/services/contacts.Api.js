d0e9b299406df726c52db1d183aa1e8417429f2a
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-09-18 12:01:54 +0200

    added apis

--- src/services/contacts.Api.js
+++ src/services/contacts.Api.js
@@ -1,20 +1,19 @@
-import axios from 'axios';
+import { backendAPI } from './baseUrl';
 
-export const contactAPI = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com/',
-});
+export const getContacts = async () => {
+  const { data } = await backendAPI.get('/contacts');
+  return data;
+};
+export const addContacts = async contact => {
+  const { data } = await backendAPI.post('/contacts', contact);
+  return data;
+};
+export const deleteContacts = async id => {
+  const { data } = await backendAPI.delete(`/contacts/${id}`);
+  return data;
+};
+export const updateContacts = async id => {
+  const { data } = await backendAPI.patch(`/contacts/${id}`);
+  return data;
+};
 
-// export const getContacts = async () => {
-//   const { data } = await contactAPI.get(`${BASEURL}/contacts`);
-//   return data;
-// };
-
-// export const addContacts = async contact => {
-//   const { data } = await contactAPI.post(`${BASEURL}/contacts`, contact);
-//   return data;
-// };
-
-// export const deleteContacts = async id => {
-//   const { data } = await contactAPI.delete(`${BASEURL}/contacts/${id}`);
-//   return data;
-// };
