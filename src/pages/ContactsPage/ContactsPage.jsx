8e41f8bb38291093d01b06853ce07ac5b5f4008d
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-09-18 17:15:51 +0200

    added homepage and contactspage

---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,25 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+
+const { useEffect } = require('react');
+const { useDispatch } = require('react-redux');
+const { getContactsThunk } = require('redux/contactsThunk');
+
+export const Contacts = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getContactsThunk);
+  }, [dispatch]);
+  return (
+    <div>
+      <ContactForm />
+      <div>
+        <Filter />
+        <ContactList />
+      </div>
+    </div>
+  );
+};
